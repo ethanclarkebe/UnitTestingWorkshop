4103839d5f3dd28cf7a66a2542377229a03f588a
Ethan Clarke <ethan.clarke@bearingpoint.com>
2024-10-24 15:28:40 +0100

    another rename

--- UnitTests/Test/BasicOrderControllerTests/BasicOrderControllerTests.cs
+++ UnitTests/Test/BasicOrderControllerTests/BasicOrderControllerTests.cs
@@ -3,7 +3,7 @@ using ChipShop.Controllers.Interfaces;
 using ChipShop.Enums;
 using ChipShop.Models;
 
-namespace ChipShop.Test.Test.BasicKitchenTests
+namespace ChipShop.Test.Test.BasicOrderControllerTests
 {
     public class BasicOrderControllerTests
     {
